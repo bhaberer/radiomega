99dce485b19afd056bf4aa84c7a798350b535fe6
Brian Haberer <bhaberer@gmail.com>
2014-02-14 11:43:18 -0800

    Setlist show view, Playlist support for sets

--- app/models/setlist.rb
+++ app/models/setlist.rb
@@ -9,4 +9,14 @@ class Setlist < ActiveRecord::Base
   def self.for_date(time)
     find_or_create_by(title: time.strftime('%F'))
   end
+
+  def first_song
+    songs.order('time').first.youtube_id
+  end
+
+  def playlist
+    playlist = songs.order('time').map(&:youtube_id)[1..-1]
+    playlist = [playlist] unless playlist.is_a?(Array)
+    playlist.join(',')
+  end
 end
